19313f90355856a95cfff726235be61636ea79e2
Timmeex <timescher96gmail.com>
2021-06-28 00:41:11 +0200

    commit 1

---
+++ src/test/java/com/example/amazon_demo/AmazonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazon_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
