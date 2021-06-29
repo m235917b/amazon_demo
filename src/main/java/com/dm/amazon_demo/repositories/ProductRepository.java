8074fd229c0a9b53a0b4cc1c4fabd4d41e8ed818
Timmeex <timescher96gmail.com>
2021-06-29 15:30:44 +0200

    get products

--- src/main/java/com/dm/amazon_demo/repositories/ProductRepository.java
+++ src/main/java/com/dm/amazon_demo/repositories/ProductRepository.java
@@ -2,10 +2,16 @@ package com.dm.amazon_demo.repositories;
 
 import com.dm.amazon_demo.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @SuppressWarnings("unused")
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("select p from Product p where p.id = ?1")
+    List<Product> getAllBy(int id);
+
 
 }
--- src/main/java/com/dm/amazon_demo/services/ProductService.java
+++ src/main/java/com/dm/amazon_demo/services/ProductService.java
@@ -1,9 +1,36 @@
 package com.dm.amazon_demo.services;
 
+import com.dm.amazon_demo.entities.Customer;
+import com.dm.amazon_demo.entities.Product;
+import com.dm.amazon_demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @SuppressWarnings("unused")
 public class ProductService {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional
+    public List<Product> getProducts() {
+
+        return productRepository.findAll();
+    }
+
+    @Transactional
+    public Optional<Product> getProductById(int id) {
+        final Product product = new Product();
+        product.setId(id);
+
+        return productRepository.findOne(Example.of(product, ExampleMatcher.matchingAll()));
+
+    }
 }
